c1f74f8630983c6fc76784d05f348e3ba1961b37
pedrohsn <pedrohsn.developer@gmail.com>
2016-11-28 13:01:41 -0200

    finalizacao de funcionalidade do corte

--- js/cut/cutting.js
+++ js/cut/cutting.js
@@ -1,21 +1,15 @@
-dragging = false;
-firstDrag = false;
-dragresize = false;
+dragimage = false;
+
+dragresize = false; // habilitador do move da barra de controle de altura
+barraclick= null; // altura inicial do click para arrastar a barra
+alturabox = null;
 
 proporcao = 100; // proporcao da imagem para o scroll
 
-imgX = null; // posicao da imagem na tela
-imgY = null; // posicao da imagem na tela
-
 stateX = null; // local inicial do click e arraste
 stateY = null; // local inicial do click e arraste
-
-DIFX = null;
-DIFY = null;
-
-alturaClick = null;
-alturaImagem = null;
-
+currentleft = null; // valor da posicao atual da imagem
+currenttop = null; // valor da posicao atual da imagem
 
 $(document).ready(function(){
 
@@ -37,121 +31,93 @@ $(document).ready(function(){
     // click para hablitar a edicao do corte
     $(document.body).on('click', '.cut-img', function(event){
         $(this).closest('.box-img').find('.img-materia').toggleClass('img-materia-cut')
-        $(this).css('color', 'red')
+        $(this).toggleClass('color-red')
+        $(this).closest('.box-img-crop').parent().find('.ctrl-height').toggle()
     });
 
-    // controlador do ZOOM IN e ZOOM OUT
+    //-------------------------------------------------//
+    //        controlador do ZOOM IN e ZOOM OUT        //
+    //-------------------------------------------------//
     $(document.body).on('wheel', '.img-materia-cut', function(event){
         event.preventDefault()
+        sizeBox = $(this.parentNode).width()
+        sizeImg = $(this).width()
 
         scroll = event.originalEvent.deltaY
-        if ( scroll < 0 ){ //diminui
+        if ( scroll < 0 ){ //aumenta
             proporcao = (proporcao+0.5)
             $(this).css('width' , ''+proporcao+'%')
             $(this).css('height', ''+proporcao+'%')
         }
-        if ( scroll > 0 ){ //aumenta
-            proporcao = (proporcao-0.5)
-            $(this).css('width' , ''+proporcao+'%')
-            $(this).css('height', ''+proporcao+'%')
+        if ( scroll > 0 ){ //diminui
+            if ( sizeImg > sizeBox ) {
+                proporcao = (proporcao-0.5)
+                $(this).css('width' , ''+proporcao+'%')
+                $(this).css('height', ''+proporcao+'%')
+            }
+        }
+    });
+
+    //-------------------------------------------------//
+    //          Controlador da BARRA DE CORTE          //
+    //-------------------------------------------------//
+    $(document.body).on('mousedown', '.ctrl-height', function(event){
+        dragresize = true
+        barraclick = event.pageY
+        alturabox = $(this.parentNode).find('.box-img-crop').height()
+        $(this).css('height', '20px')
+    });
+
+    $(document.body).on('mousemove', '.container-jornal', function(event){
+        if ( dragresize ) {
+            atual = alturabox - (barraclick - event.pageY)
+            $(this.parentNode).find('.img-materia-cut').closest('.box-img-crop').css('height', ''+atual+'px')
+        }
+    });
+
+    $(document.body).on('mouseup', '.ctrl-height', function(event){
+        dragresize = false
+        $(this).css('height', '10px')
+    });
+
+    //-------------------------------------------------//
+    //         Controlador para MOVER A IMAGEM         //
+    //-------------------------------------------------//
+
+    $(document.body).on('mousedown', '.img-materia-cut', function(event){
+        this.draggable = false
+        stateX = event.originalEvent.clientX
+        stateY = event.originalEvent.clientY
+        dragimage = true
+        currentleft = parseInt(this.style.left)
+        currenttop = parseInt(this.style.top)
+    });
+
+    $(document.body).on('mousemove', '.img-materia-cut', function(event){
+
+        if(dragimage){
+            moveX = event.originalEvent.clientX
+            moveY = event.originalEvent.clientY
+
+            newleft = currentleft+(stateX - moveX)-((stateX - moveX)*2)
+            newtop = currenttop+(stateY - moveY)-((stateY - moveY)*2)
+
+            larguraimg = this.width
+            largurabox = $(this.parentNode).width() + 10
+
+            if ( larguraimg >= largurabox ){
+                $(this).css('top', ''+newtop+'px')
+                $(this).css('left', ''+newleft+'px')
+            }else {
+                $(this).css('top', ''+newtop+'px')
+            }
+
         }
 
     });
 
-
-
-    // $(document.body).on('mousemove', '.img-materia-cut', function(event){
-    //     imagem = $('.img-materia-cut')[0]
-
-    //     // imgX = imagem.x
-    //     // imgY = imagem.y
-    //     larguraMax = $(this).closest('.box-img').width()
-
-    //     if(this.width > larguraMax){
-    //         imgX = parseInt(imagem.style.top)
-    //         imgY = parseInt(imagem.style.left)
-    //         if(dragging){
-    //             dragX = event.originalEvent.clientX
-    //             dragY = event.originalEvent.clientY
-
-    //             stepX = stateX-dragX
-    //             stepY = stateY-dragY
-
-    //             DIFX = (stepX-(stepX*2))
-    //             DIFY = (stepY-(stepY*2))
-
-    //             $(imagem).css('top', ''+DIFY+'px')
-    //             $(imagem).css('left', ''+DIFX+'px')
-    //         }
-    //     }
-
-    // });
-
-    // // controlador do local do click e abilita mover a imagem
-    // $(document.body).on('mousedown', '.img-materia-cut', function(event){
-    //     this.draggable = false
-    //     stateX = event.originalEvent.clientX
-    //     stateY = event.originalEvent.clientY
-    //     dragging = true
-    // });
-
-    // $(document.body).on('mouseup', '.img-materia-cut', function(event){
-    //     dragging = false
-    // });
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // $(document.body).on('mousedown', '.ctrl-height', function(event){
-    //     dragresize = true;
-    //     alturaImagem = $(this).closest('.box-img').find('.img-materia').height()
-    //     alturaClick = event.originalEvent.clientY
-    // });
-
-    // $(document.body).on('mouseup', '.box-img', function(event){
-    //     dragresize = false;
-    // });
-
-    // $(document.body).on('mousemove', '.box-img-cut', function(event){
-    //     if (dragresize) {
-    //         // altura do local que foi clicado
-    //         altura = event.originalEvent.clientY
-    //         difMove = alturaClick-altura
-    //         newAltura = ((alturaImagem-difMove)+30)
-    //         resizeBar = ((difMove-(difMove*2))+15)
-    //         $(this).css('height', ''+newAltura+'px')
-    //     }
-    // });
+    $(document.body).on('mouseup', '.img-materia-cut', function(event){
+        dragimage = false
+    });
 
 });
-
-// function zero(){
-
-//     dragging = false;
-//     dragresize = false;
-
-//     proporcaoX = 1024; // proporcao da imagem para o scroll
-//     proporcaoY = 768; // proporcao da imagem para o scroll
-
-//     imgX = null; // posicao da imagem na tela
-//     imgY = null; // posicao da imagem na tela
-
-//     stateX = null; // local inicial do click e arraste
-//     stateY = null; // local inicial do click e arraste
-
-//     difX = null;
-//     difY = null;
-
-//     alturaClick = null;
-//     alturaImagem = null;
-
-// }
